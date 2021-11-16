d3aa1293e6c1834c4d591421ce53f3fb6d706aef
Maryan Shapkaroski <maryan.shapkaroski@gmail.com>
2021-11-16 22:48:26 +0100

    Add project files.

---
+++ Exigy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exigy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
