60bd44f6f7ee16be87fff772acf8e23bbb138ddd
Maryan Shapkaroski <maryan.shapkaroski@gmail.com>
2021-11-17 02:15:46 +0100

    Almost

--- Exigy/Game.cs
+++ Exigy/Game.cs
@@ -14,11 +14,111 @@ namespace Exigy
             Welcome();
             Player();
             var newDeck = NewDeck();
-            Shuffle(newDeck);
+            var shuffledDeck = ShuffledMessage(newDeck);
+
+            var middleList = MiddlePile(shuffledDeck);
+            var newList =  RemoveMidlePile(shuffledDeck, middleList);
+
+        }
+
+        private static List<string> MiddlePile(List<string> shuffledDeck)
+        {
+            List<string> MiddleList = new List<string>();
+            var cards = 0;
+
+            while(cards < 4)
+            {
+                Random rnd = new Random();
+                int randomInd = rnd.Next(0, shuffledDeck.Count);
+                var element = shuffledDeck[randomInd];
+                MiddleList.Add(element);
+                cards += 1;
+            }
+            Console.WriteLine("");
+            Console.WriteLine("\nIn the middle we have: \n");
+
+            foreach (var el in MiddleList)
+            {
+                Console.Write($" {el} ");
+            }
+            return MiddleList;
+        }
+
+        public static List<string> RemoveMidlePile(List<string> shuffledDeck,List<string> middleList)
+        {
+            var circlePiles = shuffledDeck.Except(middleList).ToList();
+            Console.WriteLine("");
+            Console.WriteLine("\nIn the circle we have: \n");
+            var counter = 0;
+            foreach (var el in circlePiles)
+            {
+
+                System.Threading.Thread.Sleep(200);
+                Console.Write(el + " ");
+                counter += 1;
+                if (counter == 12)
+                {
+                    Console.WriteLine(" ");
+                    counter = 0;
+                }
+            }
+
+            return circlePiles;
         }
 
 
-        public static List<string> NewDeck()
+        public static string FindValue(string str)
+        {
+            var result = "";
+
+            switch(str){
+                case "A":
+                    result = "0";
+                    break;
+                case "1":
+                    result = "1";
+                    break;
+                case "2":
+                    result = "2";
+                    break;
+                case "3":
+                    result = "3";
+                    break;
+                case "4":
+                    result = "4";
+                    break;
+                case "5":
+                    result = "5";
+                    break;
+                case "6":
+                    result = "6";
+                    break;
+                case "7":
+                    result = "7";
+                    break;
+                case "8":
+                    result = "8";
+                    break;
+                case "9":
+                    result = "9";
+                    break;
+                case "T":
+                    result = "10";
+                    break;
+                case "J":
+                    result = "11";
+                    break;
+                case "Q":
+                    result = "12";
+                    break;
+                case "K":
+                    result = "13";
+                    break;
+            }
+            return result;
+        }
+
+        private static List<string> NewDeck()
         {
             return new Cards().CardDeck();
         }
@@ -33,7 +133,7 @@ namespace Exigy
             }
         }
 
-        public static void Player()
+        private static void Player()
         {
             Console.WriteLine("\nPlease enter your name: ");
             var name = Console.ReadLine();
@@ -45,16 +145,15 @@ namespace Exigy
                 Start = DateTime.UtcNow.ToLongDateString()
             };
 
-            Console.WriteLine($"Welcome to {newPlayer.Name}\n{newPlayer.Start} \n");
+            Console.WriteLine($"\n{newPlayer.Name} welcome!\n{newPlayer.Start} \n");
         }
 
-        private static void Shuffle(List<string> deck)
+        private static List<string> ShuffledMessage(List<string> newDeck)
         {
-            List<string> shuffle = deck.OrderBy(x => Guid.NewGuid()).ToList();
-
+            var newShuffled = Shuffle(newDeck);
             Console.WriteLine("\nPlease allow us to shuffle the cards\n");
             var counter = 0;
-            foreach (var el in shuffle)
+            foreach (var el in newShuffled)
             {
 
                 System.Threading.Thread.Sleep(200);
@@ -66,6 +165,13 @@ namespace Exigy
                     counter = 0;
                 }
             }
+
+            return newShuffled;
+        }
+
+        private static List<string> Shuffle(List<string> deck)
+        {
+            return deck.OrderBy(x => Guid.NewGuid()).ToList();
         }
 
     }
